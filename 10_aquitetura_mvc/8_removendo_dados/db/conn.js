de4b06a41bda19b2f646282575de0368d82efa0c
HenriqBatista <henriq.batista.veloso@gmail.com>
2023-09-19 20:11:00 -0300

    iniciando crud do projeto de arquitetura mvc

---
+++ 10_aquitetura_mvc/8_removendo_dados/db/conn.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("nodemvc", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+
+try {
+    sequelize.authenticate()
+    console.log("Conectado ao banco de dados MySQL")
+} catch (error) {
+    console.log(`não foi possivel conectar ao banco de dados: ${error}`)
+
+}
+
+module.exports = sequelize
